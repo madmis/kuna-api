d32613bc338d1d30c26d3396d882901d59e27222
Dmitry Machin <dmac@ciklum.com>
2017-07-21 17:52:38 +0300

    fix tests

--- tests/Endpoint/PrivateEndpointTest.php
+++ tests/Endpoint/PrivateEndpointTest.php
@@ -21,4 +21,38 @@ class PrivateEndpointTest extends \PHPUnit\Framework\TestCase
 
         new \madmis\KunaApi\Endpoint\PrivateEndpoint($client, ['publicKey' => '', 'secretKey' => null]);
     }
+
+    public function testMe()
+    {
+        $mock = $this->getMockBuilder(\madmis\KunaApi\Endpoint\PrivateEndpoint::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['sendRequest', 'getApiUrn'])
+            ->getMock();
+
+        $mock->expects($this->any())
+            ->method('sendRequest')
+            ->will($this->returnValue([
+                'email' => 'email',
+                'activated' => true,
+                'accounts' => [
+                    [
+                        'currency' => 'uah',
+                        'balance' => 1000,
+                        'locked' => 10,
+                    ]
+                ],
+            ]));
+
+        $response = $mock->me(false);
+
+        static::assertInternalType('array', $response);
+        static::assertEquals('email', $response['email']);
+        static::assertEquals('uah', $response['accounts'][0]['currency']);
+
+        $response = $mock->me(true);
+
+        static::assertInstanceOf(\madmis\KunaApi\Model\Me::class, $response);
+    }
 }
+
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
